7e5a7dd61955f5723167c96575388fa32f459be2
Scott Macfarlane <smacfarlane@chef.io>
2018-12-19 08:00:29 -0800

    Update check-bldr-toml to handle deprecations
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- bin/check-bldr-toml.sh
+++ bin/check-bldr-toml.sh
@@ -9,10 +9,28 @@
 set -eu
 requested=("$@")
 
+is_deprecated() {
+  local deprecated
+  plan_path=$1 
+
+  if [[ "$(basename $1)" == "plan.sh" ]]; then 
+    deprecated=$(source "$plan_path" && echo "$pkg_deprecated" 2>/dev/null)
+  fi 
+
+  if [[ "$deprecated" == "true" ]]; then
+    return 0
+  fi
+
+  return 1
+}
+      
+
+
 check_bldr_toml_for() {
   plan_path=$1
   plan_name=$(dirname "$plan_path")
-  if ! grep -Eq "^\[$plan_name\]" .bldr.toml
+  if ! grep -Eq "^\[$plan_name\]" .bldr.toml && 
+    ! is_deprecated "$plan_path"
   then
     echo "No entry for ${plan_name} in .bldr.toml"
     exit 1
