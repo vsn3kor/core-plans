b9b4e8dc87107c052c4ead5b79c5fc9ade261469
James Stocks <jstocks@chef.io>
2019-04-23 16:12:19 +0100

    Correct typo in gnugp-static/plan.sh
    complied -> compiled

--- gnupg-static/plan.sh
+++ gnupg-static/plan.sh
@@ -7,7 +7,7 @@ pkg_version=1.4.20
 pkg_license=('GPL-3.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="GnuPG is a complete and free implementation of the OpenPGP standard as defined by RFC4880 (also known as PGP). \
-  This is a statically complied version of gnupg."
+  This is a statically compiled version of gnupg."
 pkg_upstream_url="https://gnupg.org/"
 pkg_source=ftp://ftp.gnupg.org/gcrypt/${pkg_distname}/${pkg_distname}-${pkg_version}.tar.bz2
 pkg_distname=gnupg
